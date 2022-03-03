2070c6846d92afd4240d5d6cddead4588a1fe968
KKoder1991 <konondlovu@icloud.com>
2022-03-03 12:17:20 +0100

    Changed the text colour and change hire me to contact

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,14 +11,14 @@ export default function Navbar(){
                     </a>
                 </a>
                 <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700 flex flex-wrap items-center text-base justify-center">
-                    <a href="#projects" className="mr-5 hover:text-white">
+                    <a href="#projects" className="mr-5 text-white hover:text-red">
                         Projects
                     </a>
-                    <a href="#testimonials" className="mr-5 hover:text-white">
+                    <a href="#testimonials" className="mr-5 text-white hover:text-red">
                         Testimonials
                     </a>
-                    <a href="#contact" className="mr-5 hover:text-white">
-                        Hire Me
+                    <a href="#contact" className="mr-5 text-white hover:text-red">
+                        Contact
                     </a>
                 </nav>
             </div>
