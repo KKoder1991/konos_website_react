1130e0038ce73e7e4ac50600945d93477dbc9675
KKoder1991 <konondlovu@icloud.com>
2022-03-03 12:20:47 +0100

    removed white hover from links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,13 +11,13 @@ export default function Navbar(){
                     </a>
                 </a>
                 <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700 flex flex-wrap items-center text-base justify-center">
-                    <a href="#projects" className="mr-5 text-white hover:text-red">
+                    <a href="#projects" className="mr-5 text-white">
                         Projects
                     </a>
-                    <a href="#testimonials" className="mr-5 text-white hover:text-red">
+                    <a href="#testimonials" className="mr-5 text-white">
                         Testimonials
                     </a>
-                    <a href="#contact" className="mr-5 text-white hover:text-red">
+                    <a href="#contact" className="mr-5 text-white">
                         Contact
                     </a>
                 </nav>
